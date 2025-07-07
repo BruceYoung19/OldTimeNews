7ec442b0f88133dff0d2ce44d89c9465210f7ce1
Bruce.young19 <Bruce7645@gmail.com>
2025-07-07 20:16:14 +1200

    <TEST ENVIRONMENT> [HTML]: Description
    	- Added Testing Environment
    	- Added Data is able to be pulled via API to HTML

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask,render_template,request,jsonify
 from datetime import date
-import news
+
+import json
+import configparser
+import httpx
+import requests
+
+
+def getapikey():
+    config = configparser.ConfigParser()
+    config.read("secrets.ini")
+    return config["news"]["api_key"]
 
 app = Flask(__name__)
 
@@ -17,7 +27,21 @@ def search():
 def submit():
     subject = request.form['search_subject']
     news_data = news.searchInput(subject)
-    return render_template('submit.html',news=jsonify(news_data))
+    
+    articles = news_data.get('articles',[])
+    # TODO: Need to set a condiion for this to break if not complete.
+    
+    return render_template('submit.html',news=articles)
+
+@app.route('/test',methods=['GET'])
+def test_data():
+    api_url = "https://newsapi.org/v2/everything?q="+"Tesla"+"&apiKey="+getapikey()
+    
+    response = requests.get(api_url)
+    news_posts = response.json()    
+    # print(news_posts)
+    articles = news_posts.get('articles',[])
+    return render_template('test.html',posts=articles)
 
 if __name__ == '__main__':
     app.run(debug=True)
